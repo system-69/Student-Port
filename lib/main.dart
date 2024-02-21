eeab87966f13599faf498c0ebb4cae131789477f
System <ahmedmohamed.system69@gmail.com>
2024-02-21 18:44:53 +0200

    dsa

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'test',
+      title: 'testdasd ',
       theme: ThemeData(
         // This is the theme of your application.
         //
